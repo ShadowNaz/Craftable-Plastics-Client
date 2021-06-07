8d1fd5bb4000576d223b2c1bf83b1f72838d049f
Moody <moody@techpurity.com>
2021-06-07 16:47:56 -0500

    Changed Submission Achievement

--- src/App.js
+++ src/App.js
@@ -510,7 +510,7 @@ function Achievements() {
        <div class="card">
         <div class="card-body">
           <h2 class="card-title"><b>Inventor</b></h2>
-          <p class="card-text">Submit a Project/Idea to the Website.</p>
+          <p class="card-text">Submit a Project Photo to the Website.</p>
 			<div class="progress" style={{height: "30px"}}>
 				<div class="progress-bar progress-bar-success progress-bar-striped active" role="progressbar" aria-valuenow="40"
 				aria-valuemin="0" aria-valuemax="100" style={{width:"100%"}}>
